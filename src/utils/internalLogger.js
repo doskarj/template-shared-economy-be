55ac3b7b5a7a83323d47043193b78d78eb8e3287
Josef Doškář <josef.doskar@avast.com>
2021-04-10 13:20:06 +0200

    Do not log fo unit-test env

--- src/utils/internalLogger.js
+++ src/utils/internalLogger.js
@@ -1,13 +1,13 @@
 import { PORT, SERVER_IP, NODE_ENV, DB_URL } from './consts'
 
 export const logServerStart = () => {
-  if (NODE_ENV === 'development-test') return
+  if (NODE_ENV === 'unit-test') return
   const protocol = NODE_ENV === 'development' ? 'https' : 'http'
   const URL = `${protocol}://${SERVER_IP}:${PORT}`
   console.log('\x1b[33m' + '\nServer is listening on: ' + '\x1b[0m' + URL)
 }
 export const logEnvStatus = () => {
-  if (NODE_ENV === 'development-test') return
+  if (NODE_ENV === 'unit-test') return
   if (NODE_ENV === 'production') {
     console.log('Your are using ' + '\x1b[31m' + 'PRODUCTION' + '\x1b[0m' + ' environment. Double check, if you really want to use it please... ')
     return
@@ -18,11 +18,11 @@ export const logEnvStatus = () => {
   }
 }
 export const logDbConnectSuccess = () => {
-  if (NODE_ENV === 'development-test') return
+  if (NODE_ENV === 'unit-test') return
   console.log('\x1b[33m' + '\nDatabase connection: \n' + '\x1b[0m' + '(success) ' + DB_URL)
 }
 export const logDbConnectError = (error) => {
-  if (NODE_ENV === 'development-test') return
+  if (NODE_ENV === 'unit-test') return
   console.error(error)
   console.log('\x1b[33m' + '\nDatabase connection: \n' + '\x1b[0m' + 'Could not connect to the database. Exiting now...')
 }
